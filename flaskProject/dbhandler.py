c7f69934250927400c7183b3e64a8b9d5bf062fc
polter1337 <49211975+SoerenMLS@users.noreply.github.com>
2021-10-31 15:57:56 +0100

    added sqlite db support, and some more stuff :))))
    weeeee

---
+++ flaskProject/dbhandler.py
@@ -0,0 +1,22 @@
+import sqlite3
+import random
+from pathlib import Path
+from contextlib import closing
+
+dbpath = str(Path(__file__).resolve().parent) + "/database/users.db"
+
+
+def generate_id():
+    return random.randint(1, 500000)
+
+
+def reg_user(name, email, address):
+    userid = generate_id()
+
+    with closing(sqlite3.connect(dbpath)) as dbconnection:
+        with dbconnection:
+            with closing(dbconnection.cursor()) as cursor:
+                cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?)", (name, email, address, userid))
+
+    print(f"added {name} to the db")
+
--- flaskProject/secret_santa/routes.py
+++ flaskProject/secret_santa/routes.py
@@ -1,15 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from secret_santa import app
 from secret_santa.forms import RegistrationForm
+from dbhandler import reg_user
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def index():
     form = RegistrationForm()
-    return render_template('Index.html', title='Secret Santa', form=form)
+    if form.validate_on_submit():
+        flash(f'Form submitted, name: {form.name.data}, address: {form.address.data}, email: {form.email.data}')
+        reg_user(form.name.data, form.address.data, form.email.data)
+        return redirect('/secretsanta')
+
+    return render_template('Index.html', form=form)
 
 
-@app.route('/submit')
-def submit():
-    return 'hello world'
+@app.route('/secretsanta')
+def secret_santa():
+    return render_template('SecretSanta.html')
